beec46f0b7dbddae8d63996330f3597485eb7be2
jensen1206 <jens@harz-web.com>
2022-09-03 13:59:11 +0200

    Next Release 1.1.4 Bugfix

--- inc/register-bs-formular.php
+++ inc/register-bs-formular.php
@@ -108,7 +108,7 @@ final class RegisterBsFormularPlugin
             get_option('bs_formular_user_role'),
             'bs-formular',
             array($this, 'admin_bs_formular_page'),
-            'dashicons-email-alt', 7
+            self::get_svg_icons('email'), 100
         );
 
         add_action('load-' . $hook_suffix, array($this, 'bs_formular_load_ajax_admin_options_script'));
@@ -466,6 +466,25 @@ final class RegisterBsFormularPlugin
         wp_enqueue_script('bs5-data-table', BS_FORMULAR_PLUGIN_URL . '/assets/admin/js/tools/data-table/dataTables.bootstrap5.min.js', array(), $bs_form_version, true);
         wp_enqueue_script('bs-formular-data-js', BS_FORMULAR_PLUGIN_URL . '/assets/admin/js/admin-formulare.js', array(), $bs_form_version, true);
         wp_enqueue_script('bs-formular-table-bs-js', BS_FORMULAR_PLUGIN_URL . '/assets/admin/js/formular-table.js', array(), $bs_form_version, true);
+    }
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    private static function get_svg_icons($name): string
+    {
+        $icon = '';
+        switch ($name) {
+            case'email':
+                $icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                         </svg>';
+                break;
+            default:
+        }
+        return 'data:image/svg+xml;base64,' . base64_encode($icon);
+
     }
 }
 
